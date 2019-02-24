65cf94d473715ad695b548c0bbbf5c12d63b2860
Rufaidah <rufaidah.chan@gmail.com>
2019-02-24 22:39:56 +0700

    isi dari MainActivity

--- app/src/main/java/com/fai/myappbundle/MainActivity.kt
+++ app/src/main/java/com/fai/myappbundle/MainActivity.kt
@@ -2,11 +2,20 @@ package com.fai.myappbundle
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_login.setOnClickListener{
+            openLoginActivity(this@MainActivity, "Kalian keren")
+        }
+
+        btn_profile.setOnClickListener {
+            openProfileActivity(this@MainActivity)
+        }
     }
 }
