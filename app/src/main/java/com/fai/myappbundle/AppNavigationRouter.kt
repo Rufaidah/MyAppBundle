953daa5bb9adeaa468c3d4119b9b54a38a1665cb
Rufaidah <rufaidah.chan@gmail.com>
2019-02-24 22:16:28 +0700

    isi dari file AppNavigationRouter

--- app/src/main/java/com/fai/myappbundle/AppNavigationRouter.kt
+++ app/src/main/java/com/fai/myappbundle/AppNavigationRouter.kt
@@ -1,1 +1,28 @@
 package com.fai.myappbundle
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
+import java.security.AccessControlContext
+
+const val PARENT_PACKAGE = "com.fai"
+const val PACKAGE_PROFILE = "$PARENT_PACKAGE.profile"
+const val PACKAGE_LOGIN = "$PARENT_PACKAGE.login"
+const val BUNDLE_KEY = "bundle_key"
+
+fun openLoginActivity(context: Context, text: String){
+    try {
+        val intent = Intent(context, Class.forName("$PACKAGE_LOGIN.LoginActivity"))
+        intent.putExtra(BUNDLE_KEY, text)
+        context.startActivity(intent)
+    }catch (e : Exception){
+        Log.d("Navigation", "Activity not found")
+    }
+}
+
+fun openProfileActivity(context: Context){
+    val uri = Uri.parse("bundleapp://page/fai")
+    val intent = Intent(Intent.ACTION_VIEW, uri)
+    context.startActivity(intent)
+}
